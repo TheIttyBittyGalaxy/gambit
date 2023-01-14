7ce8f59f1bd436fc14222a86ac002c845575707b
TheIttyBittyGalaxy <joshuatiger1@gmail.com>
2023-01-14 02:20:19 +0000

    compiler: Methods for declaring and fetching from the program model

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -715,6 +715,86 @@ Json to_json(Expression expression)
     throw "Unable to serialise Expression node";
 }
 
+// PROGRAM MODEL METHODS //
+
+string identity_of(Scope::LookupValue value)
+{
+    if (IS_PTR(value, NativeType))
+        return AS_PTR(value, NativeType)->identity;
+
+    if (IS_PTR(value, EnumType))
+        return AS_PTR(value, EnumType)->identity;
+
+    if (IS_PTR(value, Entity))
+        return AS_PTR(value, Entity)->identity;
+
+    // FIXME: Throw a meaningful error object instead of a string
+    throw "Cannot get identity of Scope::LookupValue";
+}
+
+bool directly_declared_in_scope(ptr<Scope> scope, string identity)
+{
+    return scope->lookup.find(identity) != scope->lookup.end();
+}
+
+bool declared_in_scope(ptr<Scope> scope, string identity)
+{
+    while (!directly_declared_in_scope(scope, identity) && !scope->parent.expired())
+        scope = ptr<Scope>(scope->parent);
+
+    return directly_declared_in_scope(scope, identity);
+}
+
+void declare(ptr<Scope> scope, Scope::LookupValue value)
+{
+    string identity = identity_of(value);
+
+    // FIXME: Throw a meaningful error object instead of a string
+    if (directly_declared_in_scope(scope, identity))
+        throw "Cannot declare " + identity + " in scope, as " + identity + " already exists.";
+
+    scope->lookup.insert({identity, value});
+}
+
+Scope::LookupValue fetch(ptr<Scope> scope, string identity)
+{
+    while (!directly_declared_in_scope(scope, identity) && !scope->parent.expired())
+        scope = ptr<Scope>(scope->parent);
+
+    if (directly_declared_in_scope(scope, identity))
+        return scope->lookup.at(identity);
+
+    // FIXME: Throw a meaningful error object instead of a string
+    throw "Cannot fetch " + identity + " in scope, as it does not exist.";
+}
+
+ptr<NativeType> fetch_native_type(ptr<Scope> scope, string identity)
+{
+    auto found = fetch(scope, identity);
+    if (!IS_PTR(found, NativeType))
+        // FIXME: Throw a meaningful error object instead of a string
+        throw "'" + identity + "' is not a NativeType";
+    return AS_PTR(found, NativeType);
+}
+
+ptr<EnumType> fetch_enum_type(ptr<Scope> scope, string identity)
+{
+    auto found = fetch(scope, identity);
+    if (!IS_PTR(found, EnumType))
+        // FIXME: Throw a meaningful error object instead of a string
+        throw "'" + identity + "' is not a EnumType";
+    return AS_PTR(found, EnumType);
+}
+
+ptr<Entity> fetch_entity(ptr<Scope> scope, string identity)
+{
+    auto found = fetch(scope, identity);
+    if (!IS_PTR(found, Entity))
+        // FIXME: Throw a meaningful error object instead of a string
+        throw "'" + identity + "' is not an Entity";
+    return AS_PTR(found, Entity);
+}
+
 // PARSER //
 
 class Parser
