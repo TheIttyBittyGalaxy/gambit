4a9a198f4aa4018c445f58afde5a1105e5c7932c
TheIttyBittyGalaxy <joshuatiger1@gmail.com>
2023-01-15 20:08:33 +0000

    compiler: Move token code to it's own unit

---
+++ compiler/token.h
@@ -0,0 +1,86 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <map>
+#include <regex>
+#include <string>
+using namespace std;
+
+struct Token
+{
+    enum Kind
+    {
+        Line,
+
+        Equal,
+        NotEqual,
+        LessThanEqual,
+        GreaterThanEqual,
+
+        Add,
+        Sub,
+        Mul,
+        Div,
+        Dot,
+        Comma,
+        Colon,
+        Question,
+        Assign,
+        Hash,
+        ParenL,
+        ParenR,
+        CurlyL,
+        CurlyR,
+        SquareL,
+        SquareR,
+        TrigL,
+        TrigR,
+
+        KeyEntity,
+        KeyEnum,
+        KeyExtend,
+        KeyFn,
+        KeyStatic,
+        KeyState,
+        KeyProperty,
+        KeyBreak,
+        KeyContinue,
+        KeyElse,
+        KeyFor,
+        KeyIf,
+        KeyIn,
+        KeyLoop,
+        KeyMatch,
+        KeyReturn,
+        KeyUntil,
+        KeyChoose,
+        KeyFilter,
+        KeyTransform,
+        KeyAnd,
+        KeyOr,
+        KeyNot,
+
+        Boolean,
+        Number,
+        String,
+        Identity,
+    };
+
+    Kind kind;
+    string str;
+    size_t line;
+    size_t column;
+
+    Token(Kind kind, string str, size_t line, size_t column) : kind(kind),
+                                                               str(str),
+                                                               line(line),
+                                                               column(column) {}
+};
+
+string to_string(Token t);
+
+extern const map<Token::Kind, string> token_name;
+extern const map<Token::Kind, regex> token_match_rules;
+extern const map<string, Token::Kind> keyword_match_rules;
+
+#endif
