18c248fdd7cfa232a1862853042622d10cf81fe0
TheIttyBittyGalaxy <joshuatiger1@gmail.com>
2023-01-15 20:46:35 +0000

    compiler: Move lexing code into it's own unit

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include "token.h"
 #include "utilty.h"
+#include "lexing.h"
 #include "errors.h"
 using namespace std;
 
@@ -138,141 +139,6 @@ string json_str(Json json, size_t depth = 1)
     }
 
     throw "Cannot serialise invalid JSON value.";
-}
-
-// LEXING //
-
-vector<Token> generate_tokens(string src)
-{
-    vector<Token> tokens;
-    size_t line = 1;
-    size_t column = 1;
-    size_t position = 0;
-    string sub = src;
-
-    auto advance = [&](int amt)
-    {
-        column += amt;
-        position += amt;
-        sub = src.substr(position);
-    };
-
-    auto advance_line = [&]()
-    {
-        line += 1;
-        column = 1;
-        position += 1;
-        sub = src.substr(position);
-    };
-
-    size_t multi_line_comment_nesting = 0;
-    bool is_line_comment = false;
-    bool panic_mode = false;
-
-    while (position < src.length())
-    {
-        bool error_occurred = false;
-        string next = sub.substr(0, 1);
-        string next_two = sub.substr(0, 2);
-
-        if (is_line_comment)
-        {
-            if (next == "\n")
-            {
-                tokens.emplace_back(Token::Line, "\n", line, column);
-                advance_line();
-                is_line_comment = false;
-            }
-            else
-            {
-                advance(1);
-            }
-        }
-
-        else if (next_two == "/*")
-        {
-            multi_line_comment_nesting++;
-            advance(2);
-        }
-
-        else if (multi_line_comment_nesting > 0)
-        {
-            if (next_two == "*/")
-            {
-                multi_line_comment_nesting--;
-                advance(2);
-            }
-            else if (next == "\n")
-            {
-                advance_line();
-            }
-            else
-            {
-                advance(1);
-            }
-        }
-
-        else if (next_two == "//")
-        {
-            is_line_comment = true;
-            advance(2);
-        }
-
-        else if (next == "\n")
-        {
-            tokens.emplace_back(Token::Line, "\n", line, column);
-            advance_line();
-        }
-
-        else if (next == " " || next == "\t")
-        {
-            advance(1);
-        }
-
-        else
-        {
-            bool character_parsed = false;
-            for (auto rule : token_match_rules)
-            {
-                smatch info;
-                if (regex_search(sub, info, rule.second, regex_constants::match_continuous))
-                {
-                    Token::Kind kind = rule.first;
-                    string str = info.str();
-
-                    if (kind == Token::Identity)
-                    {
-                        for (auto key_rule : keyword_match_rules)
-                        {
-                            if (str == key_rule.first)
-                            {
-                                kind = key_rule.second;
-                                break;
-                            }
-                        }
-                    }
-
-                    tokens.emplace_back(kind, str, line, column);
-                    advance(info.length());
-                    character_parsed = true;
-                    break;
-                }
-            }
-
-            if (!character_parsed)
-            {
-                if (!panic_mode)
-                    emit_error("Unrecognised character " + next, line, column); // FIXME: Make this a proper error message.}
-                advance(1);
-                panic_mode = true;
-                error_occurred = true;
-            }
-        }
-
-        panic_mode = error_occurred;
-    }
-
-    return tokens;
 }
 
 // PROGRAM MODEL //
